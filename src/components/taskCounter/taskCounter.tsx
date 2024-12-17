4e2c6768de40a653dd8a5f603196f25c34e62c2b
Geetha R Kumar <geetha2work@gmail.com>
2024-12-17 17:42:37 -0500

    Made the task counter and label dynamic using typescript and proptypes

--- src/components/taskCounter/taskCounter.tsx
+++ src/components/taskCounter/taskCounter.tsx
@@ -1,7 +1,18 @@
 import React, { FC,ReactElement } from 'react';
 import { Avatar, Box, Typography } from '@mui/material';
+import { ITaskCounter } from './interfaces/ITaskCounter';
+import { Status } from '../CreateTaskForm/enums/Status';
+import { emitCorrectBorderColor } from './helpers/emitCorrectBorderColor';
+import { emitCorrectLabel } from './helpers/emitCorrectLabel';
+import PropTypes from 'prop-types';
 
-export const TaskCounter: FC = (): ReactElement => {
+export const TaskCounter: FC<ITaskCounter> = (
+    props,
+): ReactElement => {
+
+    //Destructure the props with default values:
+    const { status=Status.completed, count=0 } = props;
+
     return (
         <>
         <Box 
@@ -17,10 +28,10 @@ export const TaskCounter: FC = (): ReactElement => {
                     width: '96px',
                     height: '96px',
                     marginBottom: '16px',
-                    borderColor: 'warning.light',
+                    borderColor: `${emitCorrectBorderColor(status)}`,
                 }}
             >
-                <Typography color="#ffffff" variant="h4">10</Typography>
+                <Typography color="#ffffff" variant="h4">{count}</Typography>
             </Avatar>
             <Typography 
                 color="#ffffff"
@@ -28,10 +39,16 @@ export const TaskCounter: FC = (): ReactElement => {
                 fontSize="20px"
                 variant="h5"
             >
-                Subtitle
+                {emitCorrectLabel(status)}
             </Typography>
 
         </Box>
         </>
     )
+};
+
+//Declaration of proptypes:
+TaskCounter.propTypes = {
+    count: PropTypes.number,
+    status: PropTypes.oneOf([Status.todo, Status.inProgress, Status.completed]),
 }
