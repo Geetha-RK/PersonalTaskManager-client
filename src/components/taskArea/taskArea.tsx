6d9f5d7ca5f6e9f28284ce6937d603e3c8e68391
Geetha R Kumar <geetha2work@gmail.com>
2024-12-17 23:10:37 -0500

    Created the task component

--- src/components/taskArea/taskArea.tsx
+++ src/components/taskArea/taskArea.tsx
@@ -2,6 +2,7 @@ import {FC, ReactElement } from 'react';
 import { Grid, Box } from  '@mui/material';
 import { format } from 'date-fns';
 import { TaskCounter } from '../taskCounter/taskCounter';
+import { Task } from '../task/task';
 
 export const TaskArea: FC = (): ReactElement => {
     return(
@@ -34,8 +35,9 @@ export const TaskArea: FC = (): ReactElement => {
                       xs={10}
                       md={8}
                     >
-                    <Box>Task displayed here</Box>
-                    <Box>Task displayed here</Box>
+                    <Task />
+                    <Task />
+                    <Task />
 
                 </Grid>
 
--- src/components/taskCounter/taskCounter.tsx
+++ src/components/taskCounter/taskCounter.tsx
@@ -1,4 +1,4 @@
-import React, { FC,ReactElement } from 'react';
+import { FC,ReactElement } from 'react';
 import { Avatar, Box, Typography } from '@mui/material';
 import { ITaskCounter } from './interfaces/ITaskCounter';
 import { Status } from '../CreateTaskForm/enums/Status';
